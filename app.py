48cf7f680a80176aca3afefcd282f4634982a847
3B032112 <rjoeyic@gmail.com>
2024-04-02 15:17:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ def index():
 @app.route('/about')
 def about():
     return '<h1>Here could be something about me</h1>'
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Hello, {name}!</h1>' 
+@app.route('/user/<name>/<Surname>')
+def post(name, Surname):
+    return f'<h1>Hello, {name} {Surname}!</h1>'
+if __name__ == '__main__':
+    app.run()
