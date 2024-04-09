5291eaf444bc3005833073e5d2e239f8cf0cd4cc
3B032112 <rjoeyic@gmail.com>
2024-04-09 15:24:34 +0800

    Upload templates

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Hello, World!'
-@app.route('/about')
-def about():
-    return '<h1>Here could be something about me</h1>'
+    return render_template('index.html')
 @app.route('/user/<name>')
 def user(name):
-    return f'<h1>Hello, {name}!</h1>' 
-@app.route('/user/<name>/<Surname>')
-def post(name, Surname):
-    return f'<h1>Hello, {name} {Surname}!</h1>'
-if __name__ == '__main__':
+    return render_template('user.html',name=name)
+if __name__ =='__main__':
     app.run()
