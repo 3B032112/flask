eac8524dfac37e28b52513aeec973fc4f08462ee
3B032112 <rjoeyic@gmail.com>
2024-05-07 15:22:50 +0800

    add GET methon in index.html

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__,template_folder='templates',static_url_path='/static',static_folder='static')
 @app.route('/')
+@app.route('/index',methods=['GET'])
 def index():
-    return render_template('index.html')
+    name = request.args.get('name')
+    return render_template('index.html',**locals())
 @app.route('/shopping')
 def shopping():
     return render_template('shopping.html')
