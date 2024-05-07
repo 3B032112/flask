0611b9240830da098a684d3c44a0b0664ff90dec
3B032112 <rjoeyic@gmail.com>
2024-05-07 14:37:05 +0800

    add shopping ,tickets ,welfare web

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask,render_template
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates',static_url_path='/static',static_folder='static')
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/shopping')
+def shopping():
+    return render_template('shopping.html')
+@app.route('/tickets')
+def tickets():
+    return render_template('tickets.html')
+@app.route('/welfare')
+def welfare():
+    return render_template('welfare.html')
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name)
+
 if __name__ =='__main__':
     app.run()
